d0f646302101243a1c5c96e93880e95068de4571
Abdullah3at <120069624+Abdullah3at@users.noreply.github.com>
2023-08-31 18:22:51 -0400

    r u

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Logo from "../assets/logo.svg";
-import Resume from "../assets/resume.pdf";
+import ResumePDF from "../assets/resume.pdf";
 
 const Header = () => {
   return (
@@ -27,7 +27,7 @@ const Header = () => {
                 d="M9 5l7 7-7 7"
               ></path>
             </svg>
-            <a href={Resume} download="Resume">
+            <a href={ResumePDF} download="Resume">
               Download Resume
             </a>
           </button>
